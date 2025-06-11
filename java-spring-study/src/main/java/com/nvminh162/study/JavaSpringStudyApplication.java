a07bc26149070ac01c8db7f98a1a9d61b1ad20b8
nvminh162 <nvminh162@gmail.com>
2025-06-12 00:05:44 +0700

    learn: understanding about Spring Beans

--- java-spring-study/src/main/java/com/nvminh162/study/JavaSpringStudyApplication.java
+++ java-spring-study/src/main/java/com/nvminh162/study/JavaSpringStudyApplication.java
@@ -2,12 +2,16 @@ package com.nvminh162.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class JavaSpringStudyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaSpringStudyApplication.class, args);
+        ApplicationContext abc = SpringApplication.run(JavaSpringStudyApplication.class, args);
+        for (String s : abc.getBeanDefinitionNames()) {
+            System.out.println(s);
+        }
     }
 
 }
