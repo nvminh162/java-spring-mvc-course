b412652fd721136e1416d11535ac500aaaa7bac0
nvminh162 <nvminh162@gmail.com>
2025-06-11 18:46:54 +0700

    feat: Initial project

---
+++ java-spring-laptopshop/src/main/java/com/nvminh162/javaspringlaptopshop/JavaSpringLaptopshopApplication.java
@@ -0,0 +1,13 @@
+package com.nvminh162.javaspringlaptopshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringLaptopshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringLaptopshopApplication.class, args);
+    }
+
+}
---
+++ java-spring-laptopshop/src/test/java/com/nvminh162/javaspringlaptopshop/JavaSpringLaptopshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvminh162.javaspringlaptopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringLaptopshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
