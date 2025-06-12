8f8ef9fe6263909823edf6501ddc86def76ba182
nvminh162 <nvminh162@gmail.com>
2025-06-12 23:05:01 +0700

    learn: Brief understanding of mvc pattern flow

---
+++ java-spring-mvc/src/main/java/com/nvminh162/javaspringmvc/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.nvminh162.javaspringmvc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nvminh162.javaspringmvc.service.UserService;
+
+// Spring MVC Model
+/* @Controller
+public class UserController {
+    @RequestMapping("/")
+    public String getHomePage() {
+        return "Hello from controller!";
+    }
+} */
+
+// Spring RestFul API
+@RestController
+public class UserController {
+
+    private UserService userService;
+    
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return userService.handleHello();
+    }
+} 
