05f9d3544b8b230030b2fe3d6418a7ce451e3fc4
nvminh162 <nvminh162@gmail.com>
2025-06-13 00:01:38 +0700

    learn: how to config ViewResolver JSP

--- java-spring-mvc/src/main/java/com/nvminh162/javaspringmvc/controller/UserController.java
+++ java-spring-mvc/src/main/java/com/nvminh162/javaspringmvc/controller/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
     @RequestMapping("/")
     public String getHomePage() {
         String test = this.userService.handleHello();
-        return "nvminh162.html";
+        return "nvminh162";
     }
 }
 
